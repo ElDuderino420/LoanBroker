ec6f041790399da524c4384c11679473dab566a3
ElDuderino420 <david@pfg.dk>
2017-09-22 11:14:36 +0200

    stuff

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 var soap = require('soap');
+var amqp = require('amqplib/callback_api');
+
 var url = 'http://138.68.85.24:8080/CreditScoreService/CreditScoreService?wsdl'
+var url2 = 'amqp://student:cph@datdb.cphbusiness.dk:5672'
 var args = {
     ssn: '123456-1234'
 };
-soap.createClient(url, function (err, client) {
-    //client.creditScore()
-    if (err) {
-        console.log(err)
-    } else {
-        client.creditScore(args, function (err, result) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(result);
-            }
-        });
-    }
 
+var getCreditScore = function(ssn){
+    soap.createClient(url, function (err, client) {
+        if (err) {
+            console.log(err)
+        } else {
+            client.creditScore(ssn, function (err, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(result);
+
+
+                }
+            });
+        }
+    
+    });
+};
+
+
+
+getCreditScore(args);
+
+
+amqp.connect(url2, function(err, conn){
+    conn.createChannel(function(err, ch){
+        var ex = 'cphbusiness.bankJSON';
+
+        ch.assertExchange(ex, 'fanout', {durable: false});
+
+        ch.publish(ex, 'test', new Buffer('hello------world'));
+    })
 });
---
+++ reciver.js
@@ -0,0 +1,21 @@
+var amqp = require('amqplib/callback_api');
+var url2 = 'amqp://student:cph@datdb.cphbusiness.dk:5672'
+
+
+amqp.connect(url2, function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      var ex = 'cphbusiness.bankJSON';
+  
+      ch.assertExchange(ex, 'fanout', {durable: false});
+  
+      ch.assertQueue('test', {exclusive: true}, function(err, q) {
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+        ch.bindQueue(q.queue, ex, '');
+  
+        ch.consume(q.queue, function(msg) {
+          console.log(" [x] %s", msg.content.toString());
+        }, {noAck: true});
+      });
+      ch.publish(ex, 'test', new Buffer('helloworld'));
+    });
+  });
